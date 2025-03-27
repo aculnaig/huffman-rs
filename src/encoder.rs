3bc31a3f6b43d7aa5bcd57ccaab59f388755c13a
Gianluca Cannata <gcannata23@gmail.com>
2025-03-27 17:55:43 +0100

    Update Huffman encoder

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,3 +1,7 @@
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::io::Write;
+
 use crate::huffman::HuffmanTree;
 
 pub enum HuffmanCode<T> {
@@ -10,34 +14,109 @@ pub enum HuffmanCode<T> {
 }
 
 pub trait HuffmanContext<T> {
-    fn encode(&self, input: &Vec<T>) -> Vec<u8>;
-    fn decode(&self, input: &Vec<u8>) -> Vec<T>;
+    fn encode(&mut self, input: &[T]);
+    fn decode(&mut self, input: &[u8]);
 }
 
-pub struct HuffmanClassicalCoder<T> {
+pub struct HuffmanClassicalCoder<T, W: Write> {
     tree: HuffmanTree<T>,
+    writer: W,
 }
 
-impl<T> HuffmanContext<T> for HuffmanContext<T> {
-    fn encode(&self, input: &Vec<T>) -> Vec<u8> {
-        todo!()
-    }
-
-    fn decode(&self, input: &Vec<u8>) -> Vec<T> {
-        todo!()
+impl<T: Eq + Hash + Copy + Clone, W: Write> HuffmanClassicalCoder<T, W> {
+    pub fn new(tree: HuffmanTree<T>, writer: W) -> Self {
+        Self { tree, writer }
     }
 }
 
-pub struct HuffmanCanonicalCoder<T> {
-    lengths: Vec<(T, usize)>
-}
+impl<T: Eq + Hash + Copy + Clone + Debug, W: Write> HuffmanContext<T> for HuffmanClassicalCoder<T, W> {
+    fn encode(&mut self, input: &[T]) {
+        for symbol in input {
+            if let Some(code) = self.tree.codes.get(symbol) {
 
-impl<T> HuffmanContext<T> for HuffmanCanonicalCoder<T> {
-    fn encode(&self, input: &Vec<T>) -> Vec<u8> {
-        todo!()
+                println!("{:?}", code);
+
+                match self.writer.write(&code) {
+                    Ok(_) => (),
+                    Err(_) => panic!("Error writing symbol {:?}", symbol),
+                }
+            }
+        }
+
+        self.writer.flush();
     }
 
-    fn decode(&self, input: &Vec<u8>) -> Vec<T> {
+    fn decode(&mut self, input: &[u8]) {
         todo!()
+    }
+}
+
+pub struct HuffmanCanonicalCoder<T, W: Write> {
+    tree: HuffmanTree<T>,
+    writer: W,
+}
+
+impl<T: Eq + Hash + Copy + Clone, W: Write> HuffmanCanonicalCoder<T, W> {
+    pub fn new(tree: HuffmanTree<T>, writer: W) -> Self {
+        Self { tree, writer }
+    }
+}
+
+impl<T: Eq + Hash + Copy + Clone + Debug, W: Write> HuffmanContext<T> for HuffmanCanonicalCoder<T, W> {
+    fn encode(&mut self, input: &[T]) {
+        for symbol in input {
+            if let Some(code) = self.tree.canonical_codes.get(symbol) {
+
+                println!("{:?}", code);
+
+                match self.writer.write(&code) {
+                    Ok(_) => (),
+                    Err(_) => panic!("Error writing symbol {:?}", symbol),
+                }
+            }
+        }
+
+        self.writer.flush();
+    }
+
+    fn decode(&mut self, input: &[u8]) {
+        todo!()
+    }
+}
+
+mod tests {
+    use std::io::{Cursor, Write};
+
+    use crate::encoder::{HuffmanClassicalCoder, HuffmanContext};
+    use crate::huffman::HuffmanTree;
+
+    use super::HuffmanCanonicalCoder;
+
+    #[test]
+    fn test_classical_coder() {
+        let input = "CANNATA".as_bytes();
+
+        let tree = HuffmanTree::new(input);
+        let mut coder = HuffmanClassicalCoder::new(tree, Cursor::new(Vec::new()));
+
+        coder.encode(input);
+
+        let encoded = coder.writer.into_inner();
+
+        assert_eq!(encoded, vec![1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0]) 
+    }
+
+    #[test]
+    fn test_canonical_coder() {
+        let input = "CANNATA".as_bytes();
+
+        let tree = HuffmanTree::new(input);
+        let mut coder = HuffmanCanonicalCoder::new(tree, Cursor::new(Vec::new()));
+
+        coder.encode(input);
+
+        let encoded = coder.writer.into_inner();
+
+        assert_eq!(encoded, vec![1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0])
     }
 }
--- src/huffman.rs
+++ src/huffman.rs
@@ -1,4 +1,4 @@
-use std::hash::Hash;
+use std::{collections::HashMap, fmt::Debug, hash::Hash};
 
 pub enum HuffmanNode<T> {
     Leaf {
@@ -12,7 +12,7 @@ pub enum HuffmanNode<T> {
     },
 }
 
-impl<T: Eq + Ord> HuffmanNode<T> {
+impl<T: Eq + Ord + Copy> HuffmanNode<T> {
     pub fn frequency(&self) -> u32 {
         match self {
             HuffmanNode::Leaf { frequency, .. } => *frequency,
@@ -21,52 +21,159 @@ impl<T: Eq + Ord> HuffmanNode<T> {
     }
 }
 
-impl<T: Eq + Ord> PartialEq for HuffmanNode<T> {
+impl<T: Eq + Ord + Copy> PartialEq for HuffmanNode<T> {
     fn eq(&self, other: &Self) -> bool {
         self.frequency() == other.frequency()
     }
 }
 
-impl<T: Eq + Ord> Eq for HuffmanNode<T> {}
+impl<T: Eq + Ord + Copy> Eq for HuffmanNode<T> {}
 
-impl<T: Eq + Ord> PartialOrd for HuffmanNode<T> {
+impl<T: Eq + Ord + Copy> PartialOrd for HuffmanNode<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.frequency().cmp(&other.frequency()))
     }
 }
 
-pub struct HuffmanTree<T> {
-    pub root: Option<Box<HuffmanNode<T>>>,
+impl<T: Eq + Ord + Copy> Ord for HuffmanNode<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (HuffmanNode::Leaf { symbol: s1, frequency: f1}, HuffmanNode::Leaf { symbol: s2, frequency: f2 }) => {
+                f1.cmp(f2).then(s1.cmp(s2))
+            }
+            _ => self.frequency().cmp(&other.frequency()),
+        }
+    }
 }
 
-impl<T: Eq + Ord + Clone + Hash> HuffmanTree<T> {
+pub struct HuffmanTree<T> {
+    pub root: Option<HuffmanNode<T>>,
+    pub codes: HashMap<T, Vec<u8>>,
+    pub canonical_codes: HashMap<T, Vec<u8>>,
+}
+
+impl<T: Eq + Ord + Clone + Hash + Copy + Debug> HuffmanTree<T> {
     pub fn new(input: &[T]) -> HuffmanTree<T> {
-        let mut frequency_map = std::collections::HashMap::new();
+        let mut frequency_map = std::collections::BTreeMap::new();
         for symbol in input {
             *frequency_map.entry(symbol).or_insert(0) += 1;
         }
 
         let mut heap = std::collections::BinaryHeap::new();
         for (symbol, frequency) in frequency_map {
-            heap.push(HuffmanNode::Leaf {
+            heap.push(std::cmp::Reverse(HuffmanNode::Leaf {
                 symbol: symbol.clone(),
                 frequency,
-            });
+            }));
         }
 
         while heap.len() > 1 {
-            let left = heap.pop().unwrap();
-            let right = heap.pop().unwrap();
+            let std::cmp::Reverse(left) = heap.pop().unwrap();
+            let std::cmp::Reverse(right) = heap.pop().unwrap();
             let frequency = left.frequency() + right.frequency();
-            heap.push(HuffmanNode::Internal {
+            heap.push(std::cmp::Reverse(HuffmanNode::Internal {
                 left: Box::new(left),
                 right: Box::new(right),
                 frequency,
-            });
+            }));
         }
 
-        HuffmanTree {
-            root: heap.pop(),
+        let mut huffman_tree = HuffmanTree {
+            root: heap.pop().map(|std::cmp::Reverse(node)| node),
+            codes: HashMap::new(),
+            canonical_codes: HashMap::new(),
+        };
+
+        huffman_tree.generate_codes();
+        huffman_tree.generate_canonical_codes();
+
+        huffman_tree
+    }
+    
+    fn generate_codes(&mut self) {
+        let mut codes = HashMap::<T, Vec<u8>>::new();
+
+        if let Some(root) = &self.root {
+            Self::generate_codes_recursive(root, vec![], &mut codes);
+        }
+        
+        self.codes = codes;
+    }
+
+    fn generate_codes_recursive(node: &HuffmanNode<T>, prefix: Vec<u8>, codes: &mut HashMap<T, Vec<u8>>) {
+        match node {
+            HuffmanNode::Internal { ref left, ref right, .. } => {
+                let mut left_prefix = prefix.clone();
+                left_prefix.push(0);
+                Self::generate_codes_recursive(left, left_prefix, codes);
+
+                let mut right_prefix = prefix;
+                right_prefix.push(1);
+                Self::generate_codes_recursive(right, right_prefix, codes);
+            }
+            HuffmanNode::Leaf { symbol, .. } => {
+                codes.insert(*symbol, prefix);
+            }
+        }
+    }
+
+    fn generate_canonical_codes(&mut self) {
+        let mut canonical_codes = self
+            .codes
+            .iter()
+            .map(|(symbol, code)| (*symbol, code.len()))
+            .collect::<Vec<_>>();
+
+        canonical_codes.sort_by(|a, b| {
+            a.1.cmp(&b.1)
+                .then_with(|| a.0.cmp(&b.0))
+        });
+
+        let mut current_code = vec![0; canonical_codes.first().map_or(0, |&(_, len)| len)];
+        let mut current_length = current_code.len();
+
+        for (symbol, length) in canonical_codes {
+            if length > current_length {
+                while current_code.len() < length {
+                    current_code.push(0);
+                }
+                current_length = length;
+            }
+
+            self.canonical_codes.insert(symbol, current_code.to_vec());
+
+            Self::add_one(&mut current_code);
+        }
+    }
+
+    fn add_one(code: &mut Vec<u8>) {
+        let mut carry = 1;
+
+        for bit in code.iter_mut().rev() {
+            let sum = *bit + carry;
+            *bit = sum % 2;
+            carry = sum / 2;
+
+            if carry == 0 {
+                break;
+            }
+        }
+
+        if carry == 1 {
+            code.insert(0, 1);
         }
     }
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_one() {
+        let mut bits = vec![0, 1, 0];
+        HuffmanTree::<u8>::add_one(&mut bits);
+
+        assert_eq!(bits, vec![0, 1, 1]);
+    }
+}
+
